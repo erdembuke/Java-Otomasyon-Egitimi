e23f473971e8c09b646454bdf0e51f5e1917b476
Erdem BÜKE <erdembke@gmail.com>
2023-12-27 16:20:18 +0300

    ilk class, ilk kodlamalar, Java dilinde yazim kurallari ile birlikte eklendi

--- src/main/java/Section01/_01_IlkClass.java
+++ src/main/java/Section01/_01_IlkClass.java
@@ -4,6 +4,18 @@ public class _01_IlkClass {
 
     public static void main(String[] args) { // public -> her yerden erisilebilir / void -> method geri donus tipi yok
         System.out.println("Hello World!");
+        System.out.println("Test Automation with Java");
+        System.out.println("Deneme");
 
+        // JAVA YAZIM KURALLARI
+
+        // Java harfe duyarli bir dildir, turkce karakterler kullanilmamalidir
+        // kelimelerdeki kucuk ve buyuk farkler farkli olarak algilanir
+        // Sinif isimleri ilk harfi buyuk olmalidir. iki kelime birleseerk sinif olusturulacaksa isimlerin bas
+        // harfleri buyuk olmalidir (Car - CarModels)  /  String java'da bir class'dir ilk harfi buyuk
+        // metot adlari kucuk farkle baslar. birden fazla kelime varsa basharfleri buyur
+        // (bmw() - bmwCar())
+        // camel case : bir bilesik sozcuk icindeki her kelimenin ilk sozcuk disindaki sözcüklerin ilk harfleri
+        // buyuk harfle yazilmali (int sayiNumarasi / String userInput)
     }
 }

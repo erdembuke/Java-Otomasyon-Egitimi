1db948b89465176d4bbdd28e7f4a0e179774ed3d
Erdem BÜKE <erdembke@gmail.com>
2023-12-27 16:07:40 +0300

    Proje olusturuldu ve ilk class eklendi

---
+++ src/main/java/Section01/_01_IlkClass.java
@@ -0,0 +1,9 @@
+package Section01;
+
+public class _01_IlkClass {
+
+    public static void main(String[] args) { // public -> her yerden erisilebilir / void -> method geri donus tipi yok
+        System.out.println("Hello World!");
+
+    }
+}
